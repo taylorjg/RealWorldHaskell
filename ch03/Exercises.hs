0400b26686a57f84591bf2dee878e7f6105248ae
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2014-07-10 13:24:05 +0100

    Done chapter 3 exercises 4 & 5

--- ch03/Exercises.hs
+++ ch03/Exercises.hs
@@ -19,3 +19,17 @@ listMean :: Fractional a => [a] -> a
 listMean xs = sum xs / fromIntegral (listLength xs)
 
 -- **********************************************************************
+
+-- Exercise 4
+palindrome :: [a] -> [a]
+palindrome xs = xs ++ reverse xs
+
+-- **********************************************************************
+
+-- Exercise 5
+isPalindrome :: Eq a => [a] -> Bool
+isPalindrome [] = True
+isPalindrome [x] = True
+isPalindrome (x:xs) = (x == last xs) && isPalindrome (init xs)
+
+-- **********************************************************************
