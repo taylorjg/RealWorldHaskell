d2913bf027a67a6564d7c895c1a6030d64a1b1ef
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2014-07-14 13:51:45 +0100

    Done chapter 4 exercise 1

---
+++ ch04/Exercises.hs
@@ -0,0 +1,33 @@
+-- **********************************************************************
+
+-- Exercise 1
+
+safeHead :: [a] -> Maybe a
+safeHead (x:_) = Just x
+safeHead [] = Nothing
+
+safeTail :: [a] -> Maybe [a]
+safeTail (_:xs) = Just xs
+safeTail [] = Nothing
+
+safeLast :: [a] -> Maybe a
+safeLast [] = Nothing
+safeLast (x:[]) = Just x
+safeLast (_:xs) = safeLast xs
+
+safeInit :: [a] -> Maybe [a]
+safeInit [] = Nothing
+safeInit (x:[]) = Nothing
+safeInit (x:xs) = Just (x : loop xs)
+	where
+		loop [] = []
+		loop (x:[]) = []
+		loop (x:xs) = x : loop xs
+
+-- **********************************************************************
+
+-- Exercise 2
+
+splitWith :: (a -> Bool) -> [a] -> [[a]]
+
+-- **********************************************************************
