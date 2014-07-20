5f2470b1c7d41750c6237ce80e4d71452d7dac06
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2014-07-20 19:03:12 +0100

    Done chapter 3 exercise 11

--- ch03/Exercises.hs
+++ ch03/Exercises.hs
@@ -85,4 +85,10 @@ calculateTurn (x1,y1) (x2,y2) (x3,y3)
 	| otherwise = StraightOn
 	where crossProduct = ((x2 - x1) * (y3 - y1)) - ((y2 - y1) * (x3 - x1))
 
+-- Exercise 11
+
+calculateTurns :: [Point] -> [Direction]
+calculateTurns ps@(a:b:c:_) = calculateTurn a b c : calculateTurns (tail ps)
+calculateTurns _ = []
+
 -- **********************************************************************
